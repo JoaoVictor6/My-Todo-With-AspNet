536a75a119c1bc78c1492def1034a5cbeb060f0d
João Victor <santosjoao.dev@gmail.com>
2022-11-19 15:36:08 -0300

    refactor: remake migrations and addapt dbContext
    I was not able to remove or create new migrations :(
    
    BREAKING CHANGE: this is a break change

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,11 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using MyTodo.Models;
 
+# pragma warning disable CS1591
 namespace MyTodo.Data {
+  using MyTodo.DotEnv;
   public class AppDbContext : DbContext {
+    # pragma warning disable CS8618
     public DbSet<Todo> Todos {get; set;}
   
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
-      => optionsBuilder.UseSqlite("DataSource=app.db;Cache=shared");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+      DotEnv.Load();
+      var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+      var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
+      var password = Environment.GetEnvironmentVariable("POSTGRES_PW");
+      var database = Environment.GetEnvironmentVariable("POSTGRES_DB");
+      optionsBuilder.UseNpgsql($@"Host={host};Username={user};Password={password};Database={database}");
+      // optionsBuilder.UseSqlite("DataSource=app.db;Cache=shared");
+    }
   }
 } 
