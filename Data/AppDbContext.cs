df084c7d3f74f266e46f270a57d4263fb7604c84
João Victor <santosjoao.dev@gmail.com>
2022-11-20 12:14:42 -0300

    feat: add postgresql
    dei rever num commit ao inves do pr kkkkk🤡

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,11 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using MyTodo.Models;
-
 namespace MyTodo.Data {
+  using MyTodo.DotEnv;
   public class AppDbContext : DbContext {
     public DbSet<Todo> Todos {get; set;}
   
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
-      => optionsBuilder.UseSqlite("DataSource=app.db;Cache=shared");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+      DotEnv.Load();
+      var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+      var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
+      var password = Environment.GetEnvironmentVariable("POSTGRES_PW");
+      var database = Environment.GetEnvironmentVariable("POSTGRES_DB");
+      optionsBuilder.UseNpgsql($@"Host={host};Username={user};Password={password};Database={database}");
+      // optionsBuilder.UseSqlite("DataSource=app.db;Cache=shared");
+    }
   }
 } 
